27dc729167ab2195f4af987dec6b3770237e4a79
xiangnan0811 <weibo0718@126.com>
2023-09-04 14:05:48 +0800

    update log module

--- log/logger.py
+++ log/logger.py
@@ -9,25 +9,49 @@
 """
 
 import sys
+import logging
 
 from loguru import logger
 
 from config.env import log_path
 from config.running import LOG_LEVEL
 
-Logger = logger
-log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | " \
-             "<cyan>Process-{process}</cyan>:<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-Logger.remove(0)
-Logger.add(sys.stderr, level=LOG_LEVEL, format=log_format)
-Logger.add(
-    f"{log_path}/log.log",
-    level=LOG_LEVEL,
-    format=log_format,
-    rotation="1 day",
-    retention="30 days",
-    compression="zip",
-    backtrace=True,
-    diagnose=True,
-    enqueue=True,
-)
+
+# 将 loguru 的 logger 用作一个日志处理器
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # 获取对应的 loguru 方法
+        logger_opt = logger.opt(depth=6, exception=record.exc_info)
+        logger_opt.log(record.levelname, record.getMessage())
+
+
+def init_logger(other_modulle: str|None = None, log_file_name: str|None = None):
+    """
+    初始化日志模块
+    :return:
+    """
+    # 日志格式
+    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | " \
+                "<cyan>Process-{process}</cyan>:<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    # 日志文件
+    if not log_file_name:
+        log_file_name = "log"
+    if other_modulle:
+        # 将 InterceptHandler 添加到 other_modulle 的日志处理器中
+        logging.getLogger(other_modulle).handlers = [InterceptHandler()]
+        logging.getLogger(other_modulle).setLevel(LOG_LEVEL)
+    logger.remove(0)
+    logger.add(sys.stderr, level=LOG_LEVEL, format=log_format)
+    logger.add(
+        f"{log_path}/{log_file_name}.log",
+        level=LOG_LEVEL,
+        format=log_format,
+        rotation="1 day",
+        retention="30 days",
+        compression="zip",
+        backtrace=True,
+        diagnose=True,
+        enqueue=True,
+    )
+
+    return logger
